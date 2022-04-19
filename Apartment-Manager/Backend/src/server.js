480e62244a165ad2d8e4047c640420cf6d0796c7
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-04-19 13:33:10 +0530

    Delete Apartment-Manager directory

--- Apartment-Manager/Backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./index");
-
-const connect = require("./configs/db");
-
-app.listen(4500, async function () {
-  await connect();
-  console.log("Listening on port 4500");
-});
--- Apartment-Manager/README.md
+++
@@ -1,7 +0,0 @@
-open terminal in backend folder 
-npm install
-json-server --watch db.json --port 4500 
-
-open terminal in frontend folder 
-npm install 
-npm start
